5d66eb7dd5e3f20ad2bfa407b38f7fcb51832c04
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 10:42:41 -0600

    added properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,6 @@
 node {
+    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
+
     stage("Pull Repo"){
         echo "hello"
     }
