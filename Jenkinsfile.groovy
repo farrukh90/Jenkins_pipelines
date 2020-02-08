388501031a345588561b5ee9135f29db29cda23c
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 11:35:21 -0600

    added region and tool

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,7 @@
 node {
-    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
+    properties([parameters([
+        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'), 
+        choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose a region', name: 'AMI_REGION')])])
 
     stage("Pull Repo"){
         git 'https://github.com/farrukh90/packer.git'
