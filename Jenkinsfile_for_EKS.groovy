c1119c953e7e3c3c5c7eb2addb0ba1b99b6b6d1e
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 13:17:52 -0600

    added terraform

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -2,7 +2,7 @@ node{
     stage("Pull Repo"){
         git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
     }
-    stage("stage1"){
+    stage("Download Terraform"){
         sh "terraform version"
         sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
         sh "unzip terraform_0.12.19_linux_amd64.zip"
