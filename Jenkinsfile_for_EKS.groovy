73dd9b7dad9b82f4d775ca4de4bf94218affb7b5
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 13:23:29 -0600

    added ws

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,15 +1,21 @@
 node{
     stage("Pull Repo"){
-        git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+        ws {
+            git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+        }
+        
     }
     stage("Download Terraform"){
-        sh "terraform version"
-        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
-        sh "unzip -o terraform_0.12.19_linux_amd64.zip"
-        sh "./terraform version"
+        ws {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
     stage("Set Backend"){
-        sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+        ws {
+            sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+        }
     }
     stage("stage1"){
         echo "Hello"
