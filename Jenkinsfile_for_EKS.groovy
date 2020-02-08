8154bf8e75d3581af521d93acaa812dfcf794dd7
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 12:44:04 -0600

    added echo hello

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,19 @@
+node{
+    stage("Pull Repo"){
+        git 'https://github.com/bkarakas/terraform-iaac-eks-burak'
+
+    }
+    stage("stage1"){
+        echo "Hello"
+        
+    }
+    stage("stage1"){
+        echo "Hello"
+
+        
+    }
+    stage("stage1"){
+        echo "Hello"
+
+    }
+}
