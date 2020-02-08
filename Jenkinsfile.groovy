9597e6060edf2b0936257779fd8f0bafd5a560cf
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 10:32:25 -0600

    added jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,16 @@
-
+node {
+    stage("Pull Repo"){
+        echo "hello"
+    }
+    stage("Build Image"){
+        sh "packer version"
+    }
+    stage("Send Notification to Slack"){
+        echo "Hello"
+        
+    }
+    stage("Send Email"){
+        echo "Hello"
+        
+    }
+}
