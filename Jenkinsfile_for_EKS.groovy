936bee2e25533a94ef662d6b20f83b9362462fb0
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 13:32:38 -0600

    added tmp

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -6,18 +6,18 @@ node{
         
     }
     stage("Download Terraform"){
-        ws {
+        ws ("tmp/") {
             sh "terraform version"
             sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
             sh "unzip -o terraform_0.12.19_linux_amd64.zip"
             sh "./terraform version"}
     }
     stage("Set Backend"){
-        ws ("terraform-iaac-eks-burak"){
+        ws ("tmp/"){
             pwd()
             sh "ls -l"
             sh "ls -l ../"
-            //sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+            sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
         }
     }
     stage("stage1"){
