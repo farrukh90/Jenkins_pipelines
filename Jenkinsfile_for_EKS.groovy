7c63d9f9bf3ced2a173c8cb1fc1bd0b70c39f42a
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 13:16:52 -0600

    added terraform version

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -4,6 +4,9 @@ node{
     }
     stage("stage1"){
         sh "terraform version"
+        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+        sh "unzip terraform_0.12.19_linux_amd64.zip"
+        sh "./terraform version"
     }
     stage("stage1"){
         echo "Hello"
