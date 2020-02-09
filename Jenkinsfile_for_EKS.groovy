c9347aa7d539edc240e56e4df33e43136c84e256
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 20:19:10 -0600

    asdf

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -9,14 +9,14 @@ node{
         ws ("tmp/"){
             git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
         }
-        
     }
     stage("Download Terraform"){
         ws ("tmp/") {
             sh "terraform version"
             sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
             sh "unzip -o terraform_0.12.19_linux_amd64.zip"
-            sh "./terraform version"}
+            sh "./terraform version"
+        }
     }
     stage("Set Backend"){
         ws ("tmp/"){
