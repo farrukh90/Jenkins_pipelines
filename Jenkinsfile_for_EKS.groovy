760a9955d7f80062262fa3666a1b1d6144ba7ea0
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 20:20:36 -0600

    removed terraform

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -21,7 +21,7 @@ node{
     stage("Set Backend"){
         ws ("tmp/"){
             sh "./source setenv.sh configurations/dev/us-west-2/dev.tfvars"
-            sh "./terraform init"
+            //sh "./terraform init"
         }
     }
     stage("Plan"){
