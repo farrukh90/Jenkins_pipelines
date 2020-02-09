5986da174b7dc10ad1507efff6d63fe487d77d46
farrukh90 <farrukhsadykov@gmail.com>
2020-02-08 20:12:41 -0600

    added cd

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -8,7 +8,7 @@ node{
     stage("Pull Repo"){
         ws ("tmp/"){
             git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
-            sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+            sh "cd terraform-iaac-eks-burak.git; source setenv.sh configurations/dev/us-west-2/dev.tfvars"
         }
         
     }
