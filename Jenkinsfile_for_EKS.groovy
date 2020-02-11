65576e88e7597a4c56a837af887cc12151266ecb
farrukh90 <farrukhsadykov@gmail.com>
2020-02-10 19:21:10 -0600

    added some changes

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,15 +1,15 @@
 node{
-    properties(
-        [
-        parameters([ 
-        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please choose an environment', name: 'ENVIR'),
-        string(defaultValue: 'apply', description: 'Type your action e.g apply or destroy', name: 'ACTION', trim: false),
-        ])])
-    stage("Pull Repo"){
-        ws ("tmp/"){
-            git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
-        }
-    }
+    // properties(
+    //     [
+    //     parameters([ 
+    //     choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please choose an environment', name: 'ENVIR'),
+    //     string(defaultValue: 'apply', description: 'Type your action e.g apply or destroy', name: 'ACTION', trim: false),
+    //     ])])
+    // stage("Pull Repo"){
+    //     ws ("tmp/"){
+    //         git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+    //     }
+    // }
     stage("Check Terraform"){
         ws ("tmp/") {
             def exists = fileExists './terraform'
@@ -22,40 +22,40 @@ node{
             }
         }
     }
-    stage("Download kubectl"){
-        ws("tmp/"){
-            def exists = fileExists '/tmp/kubectl'
-            if (exists) {
-                echo 'kubectl exists'
-            } else {
-                sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl"
-                sh "chmod +x kubectl"
-                sh "cp -f kubectl /tmp"
-            }
-        }
-    }
-    // Needed for Unit Test
-    stage("Download kubectl"){
-        ws("tmp/"){
-            def exists = fileExists '/tmp/kubectl'
-            if (exists) {
-                echo 'kubectl exists'
-            } else {
-                sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl"
-                sh "chmod +x kubectl"
-                sh "cp -f kubectl /tmp"
-            }
-        }
-    }
-    stage("Set Backend"){
-        ws ("tmp/"){
-            sh "bash  setenv.sh configurations/${ENVIR}/${ENVIR}.tfvars"
-            sh "./terraform init"
-        }
-    }
-    stage("Plan"){
-        ws ("tmp/") {
-            sh "./terraform ${ACTION}  -var-file configurations/${ENVIR}/${ENVIR}.tfvars -auto-approve"
-        }
-    }
+    // stage("Download kubectl"){
+    //     ws("tmp/"){
+    //         def exists = fileExists '/tmp/kubectl'
+    //         if (exists) {
+    //             echo 'kubectl exists'
+    //         } else {
+    //             sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl"
+    //             sh "chmod +x kubectl"
+    //             sh "cp -f kubectl /tmp"
+    //         }
+    //     }
+    // }
+    // // Needed for Unit Test
+    // stage("Download kubectl"){
+    //     ws("tmp/"){
+    //         def exists = fileExists '/tmp/kubectl'
+    //         if (exists) {
+    //             echo 'kubectl exists'
+    //         } else {
+    //             sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl"
+    //             sh "chmod +x kubectl"
+    //             sh "cp -f kubectl /tmp"
+    //         }
+    //     }
+    // }
+    // stage("Set Backend"){
+    //     ws ("tmp/"){
+    //         sh "bash  setenv.sh configurations/${ENVIR}/${ENVIR}.tfvars"
+    //         sh "./terraform init"
+    //     }
+    // }
+    // stage("Plan"){
+    //     ws ("tmp/") {
+    //         sh "./terraform ${ACTION}  -var-file configurations/${ENVIR}/${ENVIR}.tfvars -auto-approve"
+    //     }
+    // }
 }
